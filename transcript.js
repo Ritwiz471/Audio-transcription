148c59e8c28f8cef5a4a75c50f3143dcfbc07af9
Pavan Adithya <pavanadithyak.s@gmail.com>
2021-06-22 13:20:42 +0530

    demo

---
+++ transcript.js
@@ -0,0 +1,35 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+
+const recognition = new SpeechRecognition();
+
+var textbox =  $("#textbox");
+
+var instructions = $("#instructions");
+ 
+var content = '';
+
+recognition.continuous = true;
+
+recognition.onstart = function (){
+    instructions.text("Recognition Started");
+}
+recognition.onspeechend = function(){
+    instructions.text("No voices heard");
+}
+recognition.onerror = function(){
+    instructions.text("There is a problem");
+}
+recognition.onresult = function(event){
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    content += transcript;
+    textbox.val(content);
+}
+
+$("#start-btn").click(function(event){
+    if(content.length){
+        content += '';
+    }
+    recognition.start();
+})
