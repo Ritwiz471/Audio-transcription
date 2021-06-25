cbed82d7b305bb5ce7496fea7b88239be50ca85d
Pavan Adithya <pavanadithyak.s@gmail.com>
2021-06-26 01:07:47 +0530

    msg

--- transcript.php
+++ transcript.php
@@ -1,6 +1,37 @@
 <?php
+session_start();
+$conn = mysqli_connect("localhost","root","","Audio");
+
+if(!$conn)
+{
+    echo "failed";
+
+}
+
+//error reporting 
+ini_set ("display_errors", "1");
+error_reporting(E_ALL);
+
+
+$batch = $_SESSION['batch'];
+$name = $_SESSION['name'];
 $trans = $_POST['trans'];
-$myfile = fopen("testfile.txt", "w");
+$filename = $name.
+$myfile = fopen("Source/$filename", "w");
 fwrite($myfile,$trans);
+
+    $query = "INSERT INTO $batch (Filename, TeacherName) values('$filename','$name')";
+    $run = mysqli_query($conn,$query);
+     //check query
+    if(!$run)
+    {
+        echo mysqli_error();
+
+    }
+    else
+    {
+        echo "sucess";
+    }
+
 fclose($myfile);
 ?>
