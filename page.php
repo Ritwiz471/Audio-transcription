4748018b26d1265c006610346d84de0c2442120a
Pavan Adithya <pavanadithyak.s@gmail.com>
2021-06-26 00:18:48 +0530

    writing

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php
-include('transcript.php');
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,6 +17,58 @@ include('transcript.php');
             <button id="stop">Stop</button>
             <p id="instructions"> Press the start button</p>
         </div>
+    <p id="dummy"></p>
     </body>
+ 
+ 
+ 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <script>
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+ 
+var content = '';
+
+recognition.continuous = true;
+
+recognition.onstart = function (){
+    document.getElementById('instructions').innerHTML="Recognition is on";
+}
+
+recognition.onspeechend = function(){
+    document.getElementById('instructions').innerHTML="NO voices heard";
+}
+recognition.onerror = function(){
+    document.getElementById('instructions').innerHTML="There is a problem";
+}
+recognition.onresult = function(event){
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    content += transcript;
     
+    document.getElementById('textbox').innerHTML= content;
+}
+
+document.getElementById('start-btn').onclick = function(event){
+    if(content.length){
+        content += " ";
+    }
+    recognition.start();
+   
+};
+document.getElementById('stop').onclick = function(event){
+    recognition.stop();
+    $.ajax({ 
+	 method: "POST", 
+	 url: "transcript.php", 
+	 data: {trans: content} 
+	}).done(function(html){		
+        document.getElementById('dummy').innerHTML=html;			
+        //function block runs if Ajax request was successful 
+	}).fail(function(html){ 
+        // function block runs if Ajax request failed 
+});
+};
+</script>
 </html>
