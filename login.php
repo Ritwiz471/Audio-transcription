22582241c69aa85ee15045f6b74bfadd36aa5e4d
Nirubama12 <nirubama.anbu@yahoo.in>
2021-06-25 20:35:03 +0300

    heloooooooooo

--- login.php
+++ login.php
@@ -17,25 +17,28 @@ $pw = $_POST['pass'];
 $role = $_POST['role'];
 $bat = $_POST['bat'];
 
-$sql1 = "SELECT * from users (WHERE ID = $id)";
+$sql1 = "SELECT * FROM users WHERE ID = $id";
 $run = mysqli_query($conn, $sql1);
 if(!$run){
-    echo mysqli_error($conn);
+    echo "error".mysqli_error($conn);
+}
+else{
+    echo "successsssss";
 }
 
 $row = mysqli_fetch_assoc($run);
-$pass = $row['pass'];
-
+$pass = $row['Password'];
 
 if($pw != $pass){
     echo "<h3>Incorrect Password</h3>";
 }
 else if($role == "Teacher" || $role = "teacher"){
-    echo "hi";
+    echo "<h3>hi</h3>";
 }
 else if($role == "Student" || $role = "student"){
     echo "heloooo";
 }
+
 
 
 
